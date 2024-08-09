93b45e29e7d6ec7510a8ef4f80bb6abfacd0fb2b
Santokh Singh <santokhpanwar@gmail.com>
2024-08-10 03:59:01 +0530

    modify view

--- app/Controller.php
+++ app/Controller.php
@@ -4,28 +4,9 @@ namespace app;
 
 class Controller
 {
-
-    protected function view($view, $data = [])
+    protected function view($filename = '', $data = [])
     {
-        ob_start();
-        require_once "../views/$view.php";
-        $content = ob_get_clean();
-        $this->renderTemplate($content, $data);
+        require_once '../view/' . $filename . '.php';
     }
-
-    public function renderTemplate($template, $data = [])
-        {
-            // Extract data array to variables
-            extract($data);
-
-            // Replace {{ }} with PHP echo escaped
-            $template = preg_replace('/\{\{\s*(.+?)\s*\}\}/', '<?php echo htmlspecialchars($1, ENT_QUOTES, "UTF-8"); ?>', $template);
-
-            // Replace {!! !!} with PHP echo unescaped
-            $template = preg_replace('/\{\!\!\s*(.+?)\s*\!\!\}/', '<?php echo $1; ?>', $template);
-
-            // Evaluate the PHP code
-            eval('?>' . $template);
-        }
-
+    
 }
